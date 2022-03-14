67f25865e3fb4545bb4087aa2fcb7c1923465699
hiepnxph <hiepnxph@gmail.com>
2022-03-14 20:42:59 +0700

    upalte

--- my-app/src/components/showInfo.jsx
+++
@@ -1,4 +0,0 @@
-const ShowInfo = (prop) => {
-    return <h1>Hello {prop.name}</h1>
-}
-export default ShowInfo;
--- my-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const root = document.querySelector('#root')
-
-
-ReactDOM.render(<div>
-  <div><App /></div>
-</div>, root)
